4ee572b2d50b04d423d922fedce95b8e552b6019
Andre07-creator <aramaldes876@gmail.com>
2023-02-12 12:04:04 -0300

    update 1.0

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ function App() {
               <div className="radio-strokes">
                 <a href="https://youtu.be/nQTeRMjiykA">Radio The Strokes</a>
               </div>
+              <div className="radio-strokes">
+                <a href="https://www.cifraclub.com.br/">TablaturaClub</a>
+              </div>
             </div>
 
             <div className="form">
@@ -31,11 +34,44 @@ function App() {
 
                 <div className="items-form">
                   <input placeholder="Digite seu email" type="text" />
-                  <input placeholder="Informe a musica que voce deseja agora" type="text" />
+                  <input
+                    placeholder="Informe a musica que voce deseja"
+                    type="text"
+                  />
                   <input type="submit" />
                 </div>
               </form>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="baseboard">
+        <div className="center">
+          <div className="content">
+            <h3>Ouça as melhores musicas</h3>
+            <p>
+              Nossa site conta com uma IA curadoria que entende o seu perfil e
+              envia as melhores musicas de acordo com o seu gosto.
+            </p>
+          </div>
+          <div className="content">
+            <h3>Radio The Strokes</h3>
+            <p>Ouça o melhor de uma das melhores bandas de rock já criada!</p>
+          </div>
+          <div className="content">
+            <h3>Estude musica</h3>
+            <p>
+              Estude de graça e onde quiser com o site{" "}
+              <a href="">TablaturaClub</a>, aprenda a tocar suas musicas
+              favoritas.
+            </p>
+          </div>
+          <div className="content">
+            <h3>Envie-nos uma sugestão</h3>
+            <p>
+              Não encontrou a musica que voce queria nos envie o nome dela para
+              ser adicionada ao site.
+            </p>
           </div>
         </div>
       </div>
